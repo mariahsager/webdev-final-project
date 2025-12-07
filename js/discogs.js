936e33e98af56c9a3cf862106b73fe50ab726aac
Mariah Sager <sager.mariah@gmail.com>
2025-12-07 05:02:18 -0800

    Add links to vinyls, add scroll to top

--- js/discogs.js
+++ js/discogs.js
@@ -17,13 +17,19 @@ export async function fetchVinylCollection() {
 			}
 			const data = await response.json();
 			pages = data.pagination ? data.pagination.pages : 1;
-			allVinyls.push(...data.releases.map(vinyl => ({
-				title: vinyl.basic_information.title,
-				artist: vinyl.basic_information.artists.map(a => a.name).join(', '),
-				label: vinyl.basic_information.labels.map(l => l.name).join(', '),
-				year: vinyl.basic_information.year,
-				image: vinyl.basic_information.cover_image
-			})));
+			allVinyls.push(...data.releases.map(vinyl => {
+				const uri = vinyl.basic_information.uri;
+				const id = vinyl.basic_information.id;
+				const url = uri ? uri : (id ? `https://www.discogs.com/release/${id}` : '');
+				return {
+					title: vinyl.basic_information.title,
+					artist: vinyl.basic_information.artists.map(a => a.name).join(', '),
+					label: vinyl.basic_information.labels.map(l => l.name).join(', '),
+					year: vinyl.basic_information.year,
+					image: vinyl.basic_information.cover_image,
+					url
+				};
+			}));
 			page++;
 		} while (page <= pages);
 		return allVinyls;

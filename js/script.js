1bb4ca82c94041e8c8b417e33ddf8bebbe6d33e3
Mariah Sager <sager.mariah@gmail.com>
2025-12-09 16:49:36 -0800

    Clean up JavaScript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+export function loadHtmlIntoContainer(htmlFile, containerId) {
+	fetch(htmlFile)
+		.then((response) => response.text())
+		.then((data) => {
+			const container = document.getElementById(containerId);
+			if (container) container.innerHTML = data;
+		});
+}
+
+export function loadNavbar() {
+	loadHtmlIntoContainer('navbar.html', 'navbar-container');
+}
+
+export function loadFooter() {
+	loadHtmlIntoContainer('footer.html', 'footer-container');
+}
+
 export function truncateText(str, maxLength) {
 	return str.length > maxLength ? str.slice(0, maxLength - 1) + "…" : str;
 }
